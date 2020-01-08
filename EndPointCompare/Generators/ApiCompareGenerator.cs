0eef3ae2eb93c3ae1eb4c812cbabc311ed108c3f
Dan Morris <dan@morris2morris.com>
2020-01-07 21:23:01 -0500

    hard coded source reports are now generating deprecated and new end point reports.

--- EndPointCompare/Generators/ApiCompareGenerator.cs
+++ EndPointCompare/Generators/ApiCompareGenerator.cs
@@ -32,24 +32,65 @@ namespace EndPointCompare.Generators
       var rightFileInfo = FileHelper.GetFileInfo(rightFilename);
       var rightFileRootInfo = FileHelper.GetDirectoryInfo(rightRootFoldername);
       Trace.WriteLine("rightFile:Exists=" + rightFileInfo.Exists + ":" + rightFileInfo.FullName);
-      var generator = new ApiReportGenerator();
-      var leftFileReport = generator.Generate(leftFileInfo, leftRootFolderInfo, leftConfigFileInfo);
+      //var generator = new ApiReportGenerator();
+      //var leftFileReport = generator.Generate(leftFileInfo, leftRootFolderInfo, leftConfigFileInfo);
+      var leftFileReport = new FileInfo("C:\\output\\retailwebapi_old.rptx");
       Trace.WriteLine("leftFile Report Generated:" + leftFileReport.FullName);
-      generator = new ApiReportGenerator();
-      var rightFileReport = generator.Generate(rightFileInfo, rightRootFolderInfo, rightConfigFileInfo); 
+      //generator = new ApiReportGenerator();
+      //var rightFileReport = generator.Generate(rightFileInfo, rightRootFolderInfo, rightConfigFileInfo);
+      var rightFileReport = new FileInfo("C:\\output\\retailwebapi_new.rptx");
       Trace.WriteLine("rightFile Report Generated:" + rightFileReport.FullName);
 
       //do compare
       var deprecatedReport = GenerateDeprecatedEndPointsReport(leftFileReport, rightFileReport);
       Trace.WriteLine("Deprecated Endpoints Report Generated:" + deprecatedReport.FullName);
-      //var newReport = GenerateNewEndPointsReport(leftFileReport, rightFileReport);
-      //Trace.WriteLine("New Endpoints Report Generated:" + newReport.FullName);
+      var newReport = GenerateNewEndPointsReport(leftFileReport, rightFileReport);
+      Trace.WriteLine("New Endpoints Report Generated:" + newReport.FullName);
 
+    }
+
+    private FileInfo GenerateNewEndPointsReport(FileInfo oldFileReport, FileInfo newFileReport)
+    {
+      var oldVersionBaseRoute = "~/api/v1-beta6/";
+      var newVersionBaseRoute = "~/api/v1/";
+      string newLineOriginal, newLine, oldLine;
+
+      var newReport = new FileInfo(Path.GetTempFileName());
+      //loop through each new record trying to find a match in the old, no match means new route/method
+      using (StreamWriter reportStream = new StreamWriter(newReport.FullName, true))
+      {
+        using (StreamReader newStream = new StreamReader(newFileReport.FullName))
+        {
+          while ((newLineOriginal = newStream.ReadLine()) != null)
+          {
+            newLine = StripBaseRouteFromString(newLineOriginal, newVersionBaseRoute);// leftLineOriginal.Substring(leftVersionBaseRoute.Length);
+            using (StreamReader oldStream = new StreamReader(oldFileReport.FullName))
+            {
+              while ((oldLine = oldStream.ReadLine()) != null)
+              {
+                oldLine = StripBaseRouteFromString(oldLine, oldVersionBaseRoute);
+                var bothLinesMatch = newLine.Equals(oldLine, StringComparison.InvariantCultureIgnoreCase);
+                if (bothLinesMatch)
+                {
+                  break;
+                }
+
+              }
+              var noMatchFound = oldLine == null;
+              if (noMatchFound)
+              {
+                reportStream.WriteLine(newLineOriginal);
+              }
+            }
+          }
+        }
+      }
+      return newReport;
     }
 
     private FileInfo GenerateDeprecatedEndPointsReport(FileInfo leftFileReport, FileInfo rightFileReport)
     {
-      var leftVersionBaseRoute = "~/api/v1/";//"-beta6/";
+      var leftVersionBaseRoute = "~/api/v1-beta6/";
       var rightVersionBaseRoute = "~/api/v1/";
       string leftLineOriginal, leftLine, rightLine;
 
